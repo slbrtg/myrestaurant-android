5cc7a68bac926daecd45512706a46690d4e07cee
saul <slbrtg@gmail.com>
2017-09-27 14:24:45 -0700

    define onrestaurantselectedlistener interface in defining custom interfaces

---
+++ app/src/main/java/com/epicodus/myrestaurants/util/OnRestaurantSelectedListener.java
@@ -0,0 +1,10 @@
+package com.epicodus.myrestaurants.util;
+
+
+import com.epicodus.myrestaurants.models.Restaurant;
+
+import java.util.ArrayList;
+
+public interface OnRestaurantSelectedListener {
+    public void onRestaurantSelected(Integer position, ArrayList<Restaurant> restaurants);
+}
